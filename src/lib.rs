b075574b88766839559d7e094aabf85c46acf169
Gabriel St Pierre <stpierreG@proton.me>
2026-09-19 19:30:32 -0400

    Create basic delay

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,19 @@ use std::sync::Arc;
 
 pub struct Cie {
     params: Arc<CieParams>,
+    buffer: Vec<f32>,
+    buffer_pointer: usize,
+    sample_rate: f32,
 }
 
 #[derive(Params)]
 struct CieParams {
     #[id = "gain"]
     pub gain: FloatParam,
+    #[id = "delay"]
+    pub delay: FloatParam,
+    #[id = "feedback"]
+    pub feedback: FloatParam,
 }
 
 impl Default for CieParams {
@@ -23,6 +30,10 @@ impl Default for CieParams {
             .with_unit(" dB")
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+            delay: FloatParam::new("Delay", 0.0, FloatRange::Linear { min: 0.0, max: 5.0 })
+                .with_unit("S"), // This might need to be longer, 5 seems good for testing now, probably shoudl be in ms as well
+            feedback: FloatParam::new("Feedback", 0.5, FloatRange::Linear { min: 0.0, max: 1.0 }) // Obviously multiply this to make it really percentage
+                .with_unit(" %"),
         }
     }
 }
@@ -31,6 +42,9 @@ impl Default for Cie {
     fn default() -> Self {
         Self {
             params: Arc::new(CieParams::default()),
+            buffer_pointer: 0,
+            sample_rate: 44100.0,
+            buffer: Vec::new(),
         }
     }
 }
@@ -68,17 +82,53 @@ impl Plugin for Cie {
         self.params.clone()
     }
 
+    fn activate(
+        &mut self,
+        audio_io_layout: &AudioIOLayout,
+        buffer_config: &BufferConfig,
+        context: &mut impl ActivateContext<Self>,
+    ) -> bool {
+        let sample_rate = buffer_config.sample_rate as usize;
+
+        self.buffer = vec![0.0; sample_rate * 5 * 2]; // Agian my 5 seconds max and both left and right ear so x2
+
+        self.sample_rate = buffer_config.sample_rate;
+
+        true
+    }
+
     fn process(
         &mut self,
         buffer: &mut Buffer,
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
+        let delay_sample = self.sample_rate * self.params.delay.value();
+
+        let feedback = self.params.feedback.value();
+
+        let length = self.buffer.len();
+
+        let mut pointer = (self.buffer_pointer as isize) - (delay_sample as isize);
+
+        if pointer < 0 {
+            pointer += length as isize;
+        }
+
+        let pointer = pointer as usize;
+
         for channel_samples in buffer.iter_samples() {
             let gain = self.params.gain.smoothed.next();
 
             for sample in channel_samples {
-                *sample *= gain;
+                self.buffer[self.buffer_pointer] = *sample;
+                // *sample *= gain;
+                *sample += (self.buffer[pointer] * feedback);
+
+                self.buffer_pointer += 1;
+                if self.buffer_pointer >= length {
+                    self.buffer_pointer = 0;
+                }
             }
         }
 
