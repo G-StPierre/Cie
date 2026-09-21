0b4e7a6f2b994e6f19ce3cbba2e233fe2e538f44
Gabriel St Pierre <stpierreG@proton.me>
2026-09-21 17:31:32 -0400

    Make delay multi-channel + Create standalone plugin

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@ use std::sync::Arc;
 
 pub struct Cie {
     params: Arc<CieParams>,
-    buffer: Vec<f32>,
+    buffer: Vec<Vec<f32>>,
     buffer_pointer: usize,
     sample_rate: f32,
 }
 
 #[derive(Params)]
 struct CieParams {
-    #[id = "gain"]
-    pub gain: FloatParam,
     #[id = "delay"]
     pub delay: FloatParam,
     #[id = "feedback"]
@@ -21,15 +19,6 @@ struct CieParams {
 impl Default for CieParams {
     fn default() -> Self {
         Self {
-            gain: FloatParam::new(
-                "Gain",
-                util::db_to_gain(0.0),
-                FloatRange::gain_range(-30.0, 30.0),
-            )
-            .with_smoother(SmoothingStyle::Logarithmic(50.0))
-            .with_unit(" dB")
-            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
             delay: FloatParam::new("Delay", 0.0, FloatRange::Linear { min: 0.0, max: 5.0 })
                 .with_unit("S"), // This might need to be longer, 5 seems good for testing now, probably shoudl be in ms as well
             feedback: FloatParam::new("Feedback", 0.5, FloatRange::Linear { min: 0.0, max: 1.0 }) // Obviously multiply this to make it really percentage
@@ -84,13 +73,13 @@ impl Plugin for Cie {
 
     fn activate(
         &mut self,
-        audio_io_layout: &AudioIOLayout,
+        _audio_io_layout: &AudioIOLayout,
         buffer_config: &BufferConfig,
-        context: &mut impl ActivateContext<Self>,
+        _context: &mut impl ActivateContext<Self>,
     ) -> bool {
         let sample_rate = buffer_config.sample_rate as usize;
 
-        self.buffer = vec![0.0; sample_rate * 5 * 2]; // Agian my 5 seconds max and both left and right ear so x2
+        self.buffer = vec![vec![0.0; sample_rate * 5]; 2]; // Again 5 second max, but 2d array makes expanding from mono -> stereo and beyond easier.
 
         self.sample_rate = buffer_config.sample_rate;
 
@@ -107,7 +96,7 @@ impl Plugin for Cie {
 
         let feedback = self.params.feedback.value();
 
-        let length = self.buffer.len();
+        let length = self.buffer[0].len();
 
         let mut pointer = (self.buffer_pointer as isize) - (delay_sample as isize);
 
@@ -115,20 +104,20 @@ impl Plugin for Cie {
             pointer += length as isize;
         }
 
-        let pointer = pointer as usize;
+        let mut pointer = pointer as usize;
 
-        for channel_samples in buffer.iter_samples() {
-            let gain = self.params.gain.smoothed.next();
-
-            for sample in channel_samples {
-                self.buffer[self.buffer_pointer] = *sample;
-                // *sample *= gain;
-                *sample += (self.buffer[pointer] * feedback);
-
-                self.buffer_pointer += 1;
-                if self.buffer_pointer >= length {
-                    self.buffer_pointer = 0;
-                }
+        for mut channel_samples in buffer.iter_samples() {
+            for (chan, sample) in channel_samples.iter_mut().enumerate() {
+                self.buffer[chan][self.buffer_pointer] = *sample;
+                *sample += self.buffer[chan][pointer] * feedback;
+            }
+            pointer += 1;
+            self.buffer_pointer += 1;
+            if self.buffer_pointer >= length {
+                self.buffer_pointer = 0;
+            }
+            if pointer >= length {
+                pointer = 0;
             }
         }
 
